ccb993113c539b59e924108270d84232e7bbe5f2
vdasilva <vini.pessoa99@gmail.com>
2023-07-26 17:16:16 -0300

    id changed to bigserial and new table posts

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,10 +1,19 @@
 CREATE DATABASE "social-app";
 
 CREATE TABLE users (
-    id bigserial NOT NULL,
+    id BIGSERIAL NOT NULL,
     username VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
     birth_date DATE,
     remaining_characters INTEGER,
+    blocked boolean,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE posts (
+    id BIGSERIAL PRIMARY KEY,
+    text VARCHAR(250) NOT NULL,
+    dateTime TIMESTAMP,
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
